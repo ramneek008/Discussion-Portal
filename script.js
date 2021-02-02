2b53689e7604b08aecf942abb1c581b5f50d143a
ramneek008 <singhramneek274@gmail.com>
2021-02-02 21:29:36 +0530

    filtering questions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var ul_ques = document.getElementById("ques_list");
 
+var id=1;
 var questions = [];
 
 var submit_ques = document.getElementById("submit_ques");
@@ -41,5 +42,54 @@ function addQuestion(){
 		localStorage.setItem("questions",JSON.stringify(questions));
 		localStorage.setItem("id",JSON.stringify(id));
 		showQuestions();
+	}
+}
+
+function filter()
+{
+	let text = document.getElementById("search").value.toLowerCase();
+	//console.log(text);
+	if(text==="")
+	{
+		//console.log(questions);
+		showQuestions();
+	}
+	else
+	{
+		showFilteredQuestions(text);
+	}
+}
+
+function showFilteredQuestions(text){
+	let size=0;
+	ul_ques.innerHTML="";
+	questions.forEach((ques) => {
+		let txt1 = ques.subject.toLowerCase();
+		let txt2 = ques.question.toLowerCase();
+		if(txt1.includes(text) || txt2.includes(text))
+		{
+			let li_ques = document.createElement("li");
+			li_ques.setAttribute("id", ques.id);
+			let h3_ques = document.createElement("h3");
+			let p_ques = document.createElement("p");
+			h3_ques.innerHTML = ques.subject;
+			p_ques.innerHTML = ques.question;
+			li_ques.appendChild(h3_ques);
+			li_ques.appendChild(p_ques);
+			ul_ques.appendChild(li_ques);
+			li_ques.addEventListener("click", showQuestionInfo);
+			size++;
+		}
+	});
+	if(size===0)
+	{
+		let li_ques = document.createElement("li");
+		let h3_ques = document.createElement("h3");
+		let p_ques = document.createElement("p");
+		h3_ques.innerHTML = "No match found";
+		p_ques.innerHTML = "";
+		li_ques.appendChild(h3_ques);
+		li_ques.appendChild(p_ques);
+		ul_ques.appendChild(li_ques);
 	}
 }
